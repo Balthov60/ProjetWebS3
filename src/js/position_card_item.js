0ffed69ceded3309b3dacf3541f0650cd19bab3e
LucasSecret <lucas.secret@orange.fr>
2018-01-12 14:47:00 +0100

    [Script] Implements random card position

--- src/js/position_card_item.js
+++ src/js/position_card_item.js
@@ -3,8 +3,13 @@ $(document).ready(function()
    var cpt=0;
    $(".card-container").each(function()
    {
-       $(this).css("margin-left", (Math.floor(Math.random() * Math.floor(10)) + 40*(cpt%2) + -3) + "%");
+       if(cpt==0)
+           $(this).css("margin-left", "6%");
+
+       else
+           $(this).css("margin-left", (Math.floor(Math.random() * Math.floor(10)) + 40*(cpt%2) + -3) + "%");
+
        cpt++;
-       alert($(this).css("height"));
+       //alert($(this).css("height"));
    })
 });
